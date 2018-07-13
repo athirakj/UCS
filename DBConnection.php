097d7ac2fc7efbff4dce7c69f41a9b5c2179eb52
athirakj <athirajayanandhankj@gmail.com>
2018-07-14 01:08:42 +0530

    testing

---
+++ DBConnection.php
@@ -0,0 +1,24 @@
+<?php
+$host       = "localhost";
+$username   = "root";
+$password   = "";
+$dbname     = "ucs_db"; // will use later
+$dsn        = "mysql:host=$host;dbname=$dbname"; // will use later
+$options    = array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+              );
+$connection = new PDO($dsn, $username, $password, $options);
+
+$sql = "SELECT * 
+				FROM users
+				";
+
+
+
+$statement = $connection->prepare($sql);
+$statement->execute();
+
+$result = $statement->fetchAll();
+echo $result;
+
+?>
